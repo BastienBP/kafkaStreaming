da8bd006c0bba47f5ae50389ddc54cfb41a377af
Bastien brunod <bastien.brunod@gmail.com>
2018-07-04 12:03:46 +0200

    feat: add java

---
+++ java/KafkaProject/src/main/java/ApplicationStream.java
@@ -0,0 +1,62 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.common.serialization.Serdes;
+
+
+import java.io.IOException;
+
+public class ApplicationStream extends StreamingConfig{
+
+    public static void main(String[] args){
+
+        ApplicationStream main = new ApplicationStream();
+        main.run();
+    }
+
+    public JsonNode parse(String stringToParse){
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JsonNode tree = mapper.readTree(stringToParse);
+            return tree;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public void print(JsonNode stringToParse){
+        System.out.println(stringToParse);
+    }
+
+
+    private void run() {
+
+        Serde<String> stringSerde = Serdes.String();
+        Serde<byte[]> byteArraySerde = Serdes.ByteArray();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<byte[], String> message = builder.stream("twitter", Consumed.with(byteArraySerde, stringSerde));
+        KStream<byte[], JsonNode> json_message = message.map((key, value) -> new KeyValue<>(key, parse(value)));
+        //KStream<byte[], JsonNode> test = message.mapValues((value)-> print(value.get("source")));
+        message.print();
+
+
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+
+
+    }
+
+}
---
+++ java/KafkaProject/src/main/java/StreamingConfig.java
@@ -0,0 +1,31 @@
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.apache.kafka.common.metrics.Sensor;
+
+public abstract class StreamingConfig{
+
+    protected StreamsConfig config;
+
+    protected StreamingConfig(){
+
+        Properties settings = new Properties();
+        settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka.streaming");
+        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        settings.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, "localhost:2181");
+        settings.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        settings.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        settings.put(StreamsConfig.TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
+        //settings.put(StreamsConfig.consumerPrefix(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG), "latest");
+        settings.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+
+        config = new StreamsConfig(settings);
+
+    }
+
+
+}
