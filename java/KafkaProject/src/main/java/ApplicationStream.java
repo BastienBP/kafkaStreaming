3d6304cabbf787cf15e550c3b53f619c3d3dfb9a
Bastien brunod <bastien.brunod@gmail.com>
2018-07-05 15:16:25 +0200

    feat: add agg and ktables

--- java/KafkaProject/src/main/java/ApplicationStream.java
+++ java/KafkaProject/src/main/java/ApplicationStream.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
@@ -8,15 +9,22 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
+
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.common.serialization.Serdes;
+//import sun.plugin2.message.Serializer;
 
 
-
+import javax.json.Json;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -25,6 +33,7 @@ public class ApplicationStream extends StreamingConfig {
     private StanfordCoreNLP pipeline;
 
     public static void main(String[] args) {
+
 
         ApplicationStream main = new ApplicationStream();
         main.run();
@@ -54,23 +63,62 @@ public class ApplicationStream extends StreamingConfig {
 
     private void run() {
 
+        Deserializer<JsonNode> jsonNodeDeserializer = new JsonDeserializer();
+        Serializer<JsonNode> jsonSerializer= new JsonSerializer();
+
         Serde<String> stringSerde = Serdes.String();
-        Serde<byte[]> byteArraySerde = Serdes.ByteArray();
+        Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonNodeDeserializer);
+
 
         ObjectMapper mapper = new ObjectMapper();
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<byte[], String> message = builder.stream("twitter", Consumed.with(byteArraySerde, stringSerde));
-        KStream<byte[], ObjectNode> json_message = message.mapValues((value) -> {
+        KStream<String, String> message = builder.stream("twitter", Consumed.with(stringSerde, stringSerde));
+        KStream<String, JsonNode> json_message = message.mapValues((value) -> {
              JsonNode node = parse(value);
              int sentiment = findSentiment(node.get("message").asText());
             ObjectNode test_copy = node.deepCopy();
             test_copy.put("sentiment",sentiment);
-            return test_copy;
+            JsonNode casted_json_message = ((JsonNode) test_copy);
 
+            return casted_json_message;
 
         });
-        json_message.print();
+
+        KGroupedStream<String, JsonNode> twitter_group_by_user = json_message.groupBy((key, value) -> value.get("nickname").asText());
+
+        KTable<String, JsonNode> group_user_twitter = twitter_group_by_user.aggregate(
+                () -> {
+                    ObjectNode node = JsonNodeFactory.instance.objectNode();
+                    node.put("negative", 0);
+                    node.put("neutral", 0);
+                    node.put("positive",0);
+
+                    return node;
+
+                },
+                (key, value, aggregate) -> {
+                    ObjectNode ag = aggregate.deepCopy();
+
+                    if (value.get("sentiment").asInt()==2){
+                        ag.put("neutral", ag.get("neutral").asLong()+1);
+                    }
+
+                    else if (value.get("sentiment").asInt()<2){
+                        ag.put("negative", ag.get("negative").asLong()+1);
+                    }
+                    else {
+                        ag.put("positive", ag.get("positive").asLong()+1);
+                    }
+                    //JsonNode ag_j = ((JsonNode) ag);
+                    return ag;
+                }, Materialized.with(stringSerde, jsonSerde));
+        //group_user_twitter.to(Serdes.String(), jsonSerde, "twitter_group_by_user");
+
+
+
+
+
 
         KafkaStreams streams = new KafkaStreams(builder.build(), config);
         streams.start();
